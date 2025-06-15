b71c84141b2d81c1e8c1ef14122fde11347cae61
Pratik Singh <141718144+pratik1412001@users.noreply.github.com>
2025-06-15 13:33:27 +0530

    login form

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// // Dark Mode Toggle
+// const toggleBtn = document.getElementById("toggleTheme");
+// const body = document.body;
+
+// // Load saved theme from localStorage
+// if (localStorage.getItem("theme") === "dark") {
+//     body.classList.add("dark-mode");
+// }
+
+// toggleBtn.addEventListener("click", () => {
+//     body.classList.toggle("dark-mode");
+//     if (body.classList.contains("dark-mode")) {
+//         localStorage.setItem("theme", "dark");
+//     } else {
+//         localStorage.setItem("theme", "light");
+//     }
+// });
+
